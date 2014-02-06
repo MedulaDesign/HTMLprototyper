133aee5488ad40e3c154214bb0f74e2f89b13e91
Nikoskip <nikoski89@hotmail.com>
2014-02-06 18:52:28 -0300

    Pequeño fix

--- Medula/HTMLprototyper/HTMLprototyper.php
+++ Medula/HTMLprototyper/HTMLprototyper.php
@@ -10,12 +10,12 @@ class HTMLprototyper
     public function __construct()
     {
         // Revisamos que el directorio de proyectos exista
-        if (!is_dir($this->projectsFolder)) {
-            throw new \Exception("Projects folder (/$this->projectsFolder) does not exists");
+        if (!is_dir($this::$projectsFolder)) {
+            throw new \Exception("Projects folder (/$this::$projectsFolder) does not exists");
         }
         // Revisamos si el directorio de proyectos tiene permisos de escritura
-        if (!is_writable($this->projectsFolder)) {
-            throw new \Exception("Projects folder (/$this->projectsFolder) is not writeable");
+        if (!is_writable($this::$projectsFolder)) {
+            throw new \Exception("Projects folder (/$this::$projectsFolder) is not writeable");
         }
         // Obtenemos la configuración para que la clase pueda tener acceso a ésta
         global $config;
@@ -29,7 +29,7 @@ class HTMLprototyper
             // Obtenemos el nombre del nuevo directorio
             $projectFolder = $this->folderName($projectName);
             // Creamos el directorio
-            mkdir($this->projectsFolder . '/' . $projectFolder);
+            mkdir($this::$projectsFolder . '/' . $projectFolder);
             // Guardamos la relacion del nombre del proyecto con el directorio
             $this->addProjecToList($projectName, $projectFolder);
             // Creamos un archivo meta-data dentro del proyecto
@@ -55,13 +55,13 @@ class HTMLprototyper
         }
         $content = str_replace('{project}', $projectName . ' - ' . $this->config['company'], $content);
         $content = str_replace('{foundation_path}', '../../foundation/' . $this->config['foundation_version'], $content);
-        $newFile = new \SPLFileObject($this->projectsFolder . '/'. $projectFolder . '/' . $fileName, 'x');
+        $newFile = new \SPLFileObject($this::$projectsFolder . '/'. $projectFolder . '/' . $fileName, 'x');
         $newFile->fwrite($content . PHP_EOL);
     }
 
     private function newProjectMetaData($projectFolder)
     {
-        $metaData = new \SPLFileObject($this->projectsFolder . '/'. $projectFolder . '/meta.txt', 'a');
+        $metaData = new \SPLFileObject($this::$projectsFolder . '/'. $projectFolder . '/meta.txt', 'a');
         $metaData->fwrite('index.html' . PHP_EOL);
         $metaData->fwrite('Created:' . date('Y-m-d h:i'). PHP_EOL);
         $metaData->fwrite('Modified:' . date('Y-m-d h:i'). PHP_EOL);
@@ -70,14 +70,14 @@ class HTMLprototyper
 
     private function addProjecToList($projectName, $projectFolder)
     {
-        $projectsList = new \SPLFileObject($this->projectsFolder . '/' . $this->projectsList, 'a');
+        $projectsList = new \SPLFileObject($this::$projectsFolder . '/' . $this->projectsList, 'a');
         $projectsList->fwrite($projectName . '::' . $projectFolder . PHP_EOL);
     }
 
     public function listProjects()
     {
         $projects = array();
-        $projectsList = new \SPLFileObject($this->projectsFolder . '/' . $this->projectsList, 'a+');
+        $projectsList = new \SPLFileObject($this::$projectsFolder . '/' . $this->projectsList, 'a+');
         foreach ($projectsList as $line) {
             if (!$projectsList->eof()) {
                 $projects[] = explode('::', trim($line));
