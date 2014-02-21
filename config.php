862e71df04ebdf2d6817d27f05d2d0f906dd29ad
Nikoskip <nikoski89@hotmail.com>
2014-02-21 17:08:17 -0300

    Re-integrando archivo de config

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'company' => 'Medula Diseño',
+	'foundation_version' => '5.0.3',
+	'lang' => 'es',
+	'timezone' => 'America/Santiago',
+	'email' => 'nikolas@medula.cl', // email1@mail.com, email2@email.com
+	'default_template' => 'template.html'
+);
